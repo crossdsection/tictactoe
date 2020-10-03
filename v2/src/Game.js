bafb471e31fb8c0ab7f85da280b9f953c87863c7
Piyush <piyush.midaas@gmail.com>
2020-10-03 14:17:01 +0530

    Code refactor

--- v2/src/Game.js
+++ v2/src/Game.js
@@ -1,71 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './Game.css';
-
-const superObj = {
-  currentChance : "user",
-  computerPlayer : {
-    playerName: "machine",
-    playerMove: "O"
-  },
-  userPlayer : {
-    playerName: "user",
-    playerMove: "X"
-  },
-  currentMatrix: []
-};
+import React from "react";
+import "./Game.css";
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentValue : ""
-    };
-    this.makeAMark = function() {
-      if( this.state.currentValue === "" ) {
-        if( superObj.currentChance === superObj.userPlayer.playerName ){
-          this.setState({ currentValue : superObj.userPlayer.playerMove});
-        } else {
-          this.setState({ currentValue : superObj.computerPlayer.playerMove});
-        }
-        superObj.currentChance = ( superObj.currentChance === superObj.userPlayer.playerName ) ? superObj.computerPlayer.playerName : superObj.userPlayer.playerName;
-      }
-    }
-  }
-
   render() {
     const heightAvailable = 0.33 * window.innerHeight;
 
+    const { currentValue, squareClicked, row, column } = this.props;
+
     return (
-      <div align="center" id={ this.props.row + "_" + this.props.column} className="square" onClick={ () => this.makeAMark() } style={ {fontSize : heightAvailable - 10} }>
-        <div style={ { width : "100%", height: "100%", lineHeight: "100%"}}> {this.state.currentValue} </div>
+      <div
+        align="center"
+        id={this.props.row + "_" + this.props.column}
+        className="square"
+        onClick={() => squareClicked(row, column)}
+        style={{ fontSize: heightAvailable - 10 }}
+      >
+        <div style={{ width: "100%", height: "100%", lineHeight: "100%" }}>
+          {currentValue}
+        </div>
       </div>
     );
   }
 }
 
 class Board extends React.Component {
+  state = {
+    currentChance: "user",
+    computerPlayer: {
+      playerName: "machine",
+      playerMove: "O",
+    },
+    userPlayer: {
+      playerName: "user",
+      playerMove: "X",
+    },
+    currentMatrix: [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ],
+  };
+
   renderSquare(i, j) {
-    return <Square key={i+j} row={i} column={j} />;
+    const { currentChance, computerPlayer, currentMatrix } = this.state;
+    return (
+      <Square
+        key={i + j}
+        row={i}
+        column={j}
+        currentValue={currentMatrix[i][j]}
+        squareClicked={this.handleSquareClick}
+      />
+    );
   }
+
+  handleSquareClick = (i, j) => {
+    this.setState((prevState) => {
+      if (prevState.currentChance === prevState.computerPlayer.playerName) {
+        prevState.currentMatrix[i][j] = prevState.computerPlayer.playerMove;
+      } else {
+        prevState.currentMatrix[i][j] = prevState.userPlayer.playerMove;
+      }
+      return {
+        currentChance:
+          prevState.currentChance === prevState.userPlayer.playerName
+            ? prevState.computerPlayer.playerName
+            : prevState.userPlayer.playerName,
+        currentMatrix: prevState.currentMatrix,
+      };
+    });
+  };
 
   renderBoardRow(i) {
     let squareItems = [];
-    for( var j = 0; j < 3; j++ ){
+    for (var j = 0; j < 3; j++) {
       squareItems.push(this.renderSquare(i, j));
     }
-    return <div key={i} className="board-row"> {squareItems} </div>;
+    return (
+      <div key={i} className="board-row">
+        {squareItems}
+      </div>
+    );
   }
-  
+
   render() {
     const boardWidth = window.innerHeight;
     const boardItems = [];
-    for( let i = 0; i < 3; i++ ){
+    for (let i = 0; i < 3; i++) {
       boardItems.push(this.renderBoardRow(i));
     }
 
     return (
-      <div className="game-board" style={{width: boardWidth}}>
+      <div className="game-board" style={{ width: boardWidth }}>
         {boardItems}
       </div>
     );
